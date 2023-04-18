17237a4ce5badef3d85f11ffb4c2ddfb037fd564
Claudinei Campos <claudineicampos95@gmail.com>
2023-04-17 22:41:22 -0300

    entrega atividade

---
+++ src/main/java/br/com/fiap/atv02/atividade02/model/Hotel.java
@@ -0,0 +1,49 @@
+package br.com.fiap.atv02.atividade02.model;
+
+public class Hotel {
+
+	private String nome;
+	private String endereco;
+	private double valorDiaria;
+	private int classificacao;
+	
+    public Hotel(String nome, String endereco, double valorDiaria, int classificacao) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.valorDiaria = valorDiaria;
+        this.classificacao = classificacao;
+    }
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return this.endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public double getValorDiaria() {
+		return this.valorDiaria;
+	}
+
+	public void setValorDiaria(double valorDiaria) {
+		this.valorDiaria = valorDiaria;
+	}
+
+	public int getClassificacao() {
+		return this.classificacao;
+	}
+
+	public void setClassificacao(int classificacao) {
+		this.classificacao = classificacao;
+	}
+
+}
---
+++ src/main/java/br/com/fiap/atv02/atividade02/model/Usuario.java
@@ -0,0 +1,49 @@
+package br.com.fiap.atv02.atividade02.model;
+
+public class Usuario {
+    
+    private String login;
+    private String senha;
+    private String nome;
+    private String email;
+
+    /* public Usuario (String login, String senha, String nome, String email) {
+        this.login = login;
+        this.senha = senha;
+        this.nome = nome;
+        this.email = email;
+    } */
+
+    public String getLogin() {
+        return this.login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return this.senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
